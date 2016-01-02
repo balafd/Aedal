a1152f69cb10df538f338e609c5182f1421035f9
Balachander M <balachander.m@freshdesk.com>
2016-01-02 22:31:51 +0530

    Added Carthage dependency manager and Alamofire framework

---
+++ Aedal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aedal
+//
+//  Created by Bala on 02/01/16.
+//  Copyright © 2016 Freshdesk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
